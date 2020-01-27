e594724aca9bf2c3e50e6bbe16755d81cb24ecc8
PwntasticKev <pwntastickevin@gmail.com>
2020-01-27 11:26:40 -0700

    scrape now grabs 1rst page oif amaozn reviews

--- uribeScrape.js
+++ uribeScrape.js
@@ -4,35 +4,27 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
-let data = [];
 async function scrapeProduct(urls) {
 	urls.map(async function(url, index) {
 		const browser = await puppeteer.launch({ headless: true });
 		const page = await browser.newPage();
 		await page.goto(url);
-		// await page.setViewport({ width: 800, height: 600 });
 		await page.waitFor(5000);
 
-		const result = await page.$$eval('.filterable-reviews-content', rows => {
+		const result = await page.$$eval('.filterable-reviews-content .a-section .review', rows => {
 			return rows.map(review => {
 				const properties = {};
 				const titleElement = review.querySelector(".review-title-content span");
 				properties.title = titleElement.innerText;
-				properties.ranking = review.querySelector(".review-title-content span");
-				// return properties;
+				const starElement = review.querySelector(".review-rating");
+				properties.ranking = starElement.innerText;
+				const reviewDateElement = review.querySelector(".review-date");
+				properties.reviewDate = reviewDateElement.innerText;
+				const authorName = review.querySelector(".a-profile-name");
+				properties.authorName = authorName.innerText;
+				return properties;
 			})
 		})
-
-		// let content = await page.evaluate(() => {
-		// 	let divs = [...document.querySelectorAll('.filterable-reviews-content')];
-		// 	return divs.map(item => {
-		// 		return item.textContent.replace(/(\r\n|\n|\r)/gm,"")
-		// 	} );
-		//   });
-		
-		// console.log({productImg, productTitle, buyBoxUsername, productPrice});
-		// data.push({ reviewText });
-		// console.log(data);
 		console.log(result);
 		await browser.close();
 	});
